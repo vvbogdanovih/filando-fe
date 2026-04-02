57e1b921cb905de6c2caeeef4e998eb7cc37607e
vvbogdanovih <vvbogdanovih@gmail.com>
2026-04-02 12:13:38 +0300

    Add production configuration with Docker support, including Dockerfile and docker-compose setup for frontend service. Update Next.js configuration for standalone output. Implement deployment script in GitHub Actions for automated builds and updates.

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+	output: 'standalone',
 	reactCompiler: true,
 	images: {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: 'filando.s3.eu-north-1.amazonaws.com',
-			},
-		],
-	},
+				hostname: 'filando.s3.eu-north-1.amazonaws.com'
+			}
+		]
+	}
 }
 
 export default nextConfig
